ace603237f839e7a084f414740b1524619052797
➺ѕเℓεɳƭ_ℓσѵε૨࿐ <rendera36@gmail.com>
2025-02-17 00:48:30 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
     CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
-    READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
+    READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
     MODE: process.env.MODE || "public",
     AUTO_VOICE: process.env.AUTO_VOICE || "true",
     AUTO_STICKER: process.env.AUTO_STICKER || "false",
@@ -18,20 +18,28 @@ module.exports = {
     ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/2a06381b260c3f096a612.jpg",
     ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M SILENT-SOBX-MD WHATSAPP BOT 😊♻️",
     ANTI_LINK: process.env.ANTI_LINK || "true",
+    ANTI_CALL: process.env.ANTI_BAD || "true",
+    PM_BLOCKER: process.env.PM_BLOCKER || "true",
+    ANTI_FAKE: process.env.ANTI_BAD || "true",
     ANTI_BAD: process.env.ANTI_BAD || "true",
     PREFIX: process.env.PREFIX || ".",
     FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
     FAKE_TYPING: process.env.FAKE_TYPING || "false",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
     CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
-    AUTO_REACT: process.env.AUTO_REACT || "true",
-    HEART_REACT: process.env.HEART_REACT || "true",
-    OWNER_REACT: process.env.OWNER_REACT || "true",
+    AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    HEART_REACT: process.env.HEART_REACT || "false",
+    OWNER_REACT: process.env.OWNER_REACT || "false",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "923154647639",
+    OWNER_NAME: process.env.OWNER_NAME || "SILENTLOVER432",
     READ_CMD: process.env.READ_CMD || "true",
+    ANTI_FAKE: process.env.ANTI_FAKE || "true",
     BOT_NAME: process.env.BOT_NAME || "➺ѕเℓεɳƭ_ℓσѵε૨࿐",
-    STATUS_REPLY: process.env.STATUS_REPLY || "`➺үσυя sтαтυs sεεη נυsт ησω вү sιℓεηт-sσвx-м∂`",
-    STATUS_REACT: process.env.STATUS_REACT || "true",
+    DEV: process.env.DEV || "923096287432",//replace with your whatsapp number        
     ANTI_VV: process.env.ANTI_VV || "true",// true for anti once view 
-    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same", // change it to 'log' if you want to resend deleted message in ib chat 
+    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same", // change it to 'same' if you want to resend deleted message in same chat 
+    STATUS_REACT: process.env.STATUS_REACT || "true",
+    STATUS_REPLY: process.env.STATUS_REPLY || "`➺үσυя sтαтυs sεεη נυsт ησω вү sιℓεηт-sσвx-м∂`",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
